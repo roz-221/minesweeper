dbeda43b8b04e51507b7644db91da7433dc55d6a
roz-221 <86081534+roz-221@users.noreply.github.com>
2025-05-03 02:37:59 -0400

    Create WelcomeWindow.h

---
+++ WelcomeWindow.h
@@ -0,0 +1,9 @@
+#ifndef WELCOME_WINDOW_H
+#define WELCOME_WINDOW_H
+
+#include <SFML/Graphics.hpp>
+#include <string>
+
+std::string runWelcomeWindow(int width, int height);
+
+#endif // WELCOME_WINDOW_H
